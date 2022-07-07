aee90ebb5d379ad268a8b217ea5211b4df3a5f82
LorenzoBertoni <lorenzo.berto99@hotmail.it>
2022-07-07 16:33:31 +0200

    refactoring JS

--- esercizio mail/js/main.js
+++ esercizio mail/js/main.js
@@ -2,12 +2,9 @@ const userMail = prompt('Inserisci la tua email');
 
 const invitedMail = ['lorenzo.bertoni@gmail.com', 'mario.rossi@gmail.com', 'luca.borghese@gmail.com'];
 
-    if (userMail == invitedMail[0]) {
-        alert('Sei tra gli invitati!')
-    } else if (userMail == invitedMail[1]){
-        alert('Sei tra gli invitati!')
-    } else if(userMail == invitedMail[2]){
+    if (userMail == invitedMail[0] || userMail == invitedMail[1] || userMail == invitedMail[2]) {
         alert('Sei tra gli invitati!')
     } else {
         alert('Non sei tra gli invitati!')
     }
+
