1b7786b428d0101c47f5b0ad022073e0e26e3ddd
LorenzoBertoni <lorenzo.berto99@hotmail.it>
2022-07-07 21:16:22 +0200

    fix: esercizo delle mail: aggiunto un ciclo che controlla i valori dell'array di "invited mail" e stampa un alert. Dichiarata una variabile vuota che costituirà la condizione (fuori dal ciclo) per la quale stampare il secondo alert.

--- esercizio mail/js/main.js
+++ esercizio mail/js/main.js
@@ -2,9 +2,17 @@ const userMail = prompt('Inserisci la tua email');
 
 const invitedMail = ['lorenzo.bertoni@gmail.com', 'mario.rossi@gmail.com', 'luca.borghese@gmail.com'];
 
-    if (userMail == invitedMail[0] || userMail == invitedMail[1] || userMail == invitedMail[2]) {
-        alert('Sei tra gli invitati!')
-    } else {
-        alert('Non sei tra gli invitati!')
+let ric;
+
+    for(let i = 0; i < invitedMail.length; i++) {
+        if (invitedMail[i] == userMail) {
+            alert('sei tra gli invitati!')
+            ric = true;
+        }
     }
 
+    if (ric != true) {
+        alert('non sei tra gli invitati!')
+    }
+
+    
